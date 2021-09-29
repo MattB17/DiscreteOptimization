6160085b423baed147369013326152f31b983b02
MattB17 <matt.buckley1755@gmail.com>
2021-09-29 12:25:39 -0400

    Adding simple c++ implementation

--- 2_knapsack/solver.py
+++ 2_knapsack/solver.py
@@ -32,7 +32,7 @@ def solve_it(input_data):
             taken[item.index] = 1
             value += item.value
             weight += item.weight
-    
+
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, taken))
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         file_location = sys.argv[1].strip()
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
+            print(input_data)
         print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
-
