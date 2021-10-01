5b145d2988d2340f8eb292291a6f90f87556e38e
MattB <matt.buckley1755@gmail.com>
2021-10-01 16:51:25 -0400

    Generalizing knapsack solution

--- 2_knapsack/methods.py
+++ 2_knapsack/methods.py
@@ -6,67 +6,69 @@ Used to define methods for solving the knapsack problem.
 """
 from BranchNode import BranchNode
 from collections import deque
+import specs
 
-def backtrack(n, capacity, dp, weights):
+def backtrack(dp, weights, capacity, n, offset):
     curr_capacity = capacity
     curr_item = n
-    taken = ["0" for _ in range(n)]
-    while (curr_capacity > 0 and curr_item > 0):
-        if (dp[curr_capacity][curr_item] != dp[curr_capacity][curr_item-1]):
-            taken[curr_item - 1] = "1"
+    taken_items = set()
+    while (curr_capacity > 0 and curr_item > offset):
+        i = curr_item - offset
+        if (dp[curr_capacity][i] != dp[curr_capacity][i-1]):
+            taken_items.add(curr_item - 1)
             curr_capacity -= weights[curr_item - 1]
         curr_item -= 1
-    return taken
+    return dp[capacity][n], taken_items
 
 
-def run_dp_knapsack(values, weights, capacity, n):
-    dp = [[0 for _ in range(n+1)] for _ in range(capacity+1)]
+def run_dp_knapsack(values, weights, capacity, n, offset):
+    dp = [[0 for _ in range(n+1 - offset)] for _ in range(capacity+1)]
     for k in range(1, capacity+1):
-        for j in range(1, n+1):
-            dp[k][j] = dp[k][j-1]
-            if weights[j-1] <= k:
-                add_item_val = dp[k - weights[j-1]][j-1] + values[j-1]
-                dp[k][j] = max(dp[k][j], add_item_val)
+        for j in range(offset+1, n+1):
+            i = j - offset
+            dp[k][i] = dp[k][i-1]
+            if weights[i-1] <= k:
+                add_item_val = dp[k - weights[i-1]][i-1] + values[i-1]
+                dp[k][i] = max(dp[k][i], add_item_val)
     return dp
 
+def stringify(solution, n):
+    taken = ["0" for _ in range(n)]
+    for item in solution:
+        taken[item] = "1"
+    return taken
 
 def dp_solver(values, weights, capacity):
     n = len(values)
-    dp = run_dp_knapsack(values, weights, capacity, n)
-    taken = backtrack(n, capacity, dp, weights)
-    return "{0} 1\n{1}".format(dp[capacity][n], " ".join(taken))
+    dp = run_dp_knapsack(values, weights, capacity, n, 0)
+    opt, solution = backtrack(dp, weights, capacity, n, 0)
+    return "{0} 1\n{1}".format(opt, " ".join(stringify(solution, n)))
 
 
-def solve_fractional_knapsack(item_tups, cutoff, capacity, n):
+def solve_fractional_knapsack(item_tups, offset, capacity, n):
     total_val = 0
     item_idx = 0
     while (capacity > 0 and item_idx < n):
-        if (item_tups[item_idx][2] <= cutoff):
-            item_idx += 1
-            continue
-        if item_tups[item_idx][1] <= capacity:
-            capacity -= item_tups[item_idx][1]
-            total_val += item_tups[item_idx][0]
-        else:
-            frac_taken = capacity / item_tups[item_idx][1]
-            total_val += frac_taken * item_tups[item_idx][0]
-            capacity = 0
+        if (item_tups[item_idx][2] >= offset):
+            if item_tups[item_idx][1] <= capacity:
+                capacity -= item_tups[item_idx][1]
+                total_val += item_tups[item_idx][0]
+            else:
+                frac_taken = capacity / item_tups[item_idx][1]
+                total_val += frac_taken * item_tups[item_idx][0]
+                capacity = 0
         item_idx += 1
     return total_val
-
-def filter_items(item_tuples, cutoff):
-    return [item_tuple for item_tuple in item_tuples
-            if item_tuple[2] > cutoff]
 
 def initialize_branch_and_bound(item_tups, capacity, n):
     first_node = BranchNode(
         set(), -1, capacity, 0,
-        solve_fractional_knapsack(item_tups, -1, capacity, n))
+        solve_fractional_knapsack(item_tups, 0, capacity, n))
     return deque([first_node])
 
 def branch_right(node, next_item, n, item_tups):
     max_val = node.val + solve_fractional_knapsack(
-        item_tups, next_item, node.cap, n - next_item - 1)
+        item_tups, next_item + 1, node.cap, n)
     right_node = BranchNode(
         node.items, next_item, node.cap, node.val, max_val)
     return right_node
@@ -76,12 +78,14 @@ def branch_left(node, next_item, n, item_tups, values, weights):
     rem_cap = node.cap - weights[next_item]
     curr_val = node.val + values[next_item]
     max_val = curr_val + solve_fractional_knapsack(
-        item_tups, next_item, rem_cap, n - next_item - 1)
+        item_tups, next_item + 1, rem_cap, n)
     left_node = BranchNode(curr_items, next_item, rem_cap, curr_val, max_val)
     return left_node
 
 def branch_and_bound(values, weights, capacity):
     n = len(values)
+    if (n * capacity <= specs.DP_MAX_SIZE):
+        return dp_solver(values, weights, capacity)
     item_tups = [(values[i], weights[i], i) for i in range(n)]
     item_tups.sort(key=lambda item_tup: item_tup[0]/item_tup[1],
                    reverse=True)
---
+++ 2_knapsack/specs.py
@@ -0,0 +1,6 @@
+"""
+Contains specification parameters used in the knapsack algorithms.
+
+"""
+
+DP_MAX_SIZE = 50000
